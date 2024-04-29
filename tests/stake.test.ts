5cd92f27eff9721d806e6368fd58faed2e1be255
psmh0 <mike@placeholdersoft.com>
2024-04-29 01:56:27 -0500

    feat: init project & check owner is multisig

--- tests/stake.test.ts
+++ tests/stake.test.ts
@@ -1,4 +1,4 @@
-
+import { principalCV } from "@stacks/transactions";
 import { describe, expect, it } from "vitest";
 
 const accounts = simnet.getAccounts();
@@ -8,14 +8,25 @@ const address1 = accounts.get("wallet_1")!;
   The test below is an example. To learn more, read the testing documentation here:
   https://docs.hiro.so/clarinet/feature-guides/test-contract-with-clarinet-sdk
 */
-
 describe("example tests", () => {
-  it("ensures simnet is well initalised", () => {
+  it("ensures simnet is well initialized", () => {
     expect(simnet.blockHeight).toBeDefined();
   });
 
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
+  it("check multisig address", () => {
+    const r1 = simnet.callReadOnlyFn(
+      "sata-vault",
+      "is-multisig",
+      [principalCV(address1)],
+      address1
+    );
+    expect(r1.result).toBeBool(false);
+    const r2 = simnet.callReadOnlyFn(
+      "sata-vault",
+      "is-multisig",
+      [principalCV("SM3R3ECP86C45ARMPYAWQ85H9B4Q4YV27YHB94QZA")],
+      address1
+    );
+    expect(r2.result).toBeBool(true);
+  });
 });
